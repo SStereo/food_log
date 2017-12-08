4b87fbfa93c6f340ddde533364c6fa4936f165b0
soeren schmidt <mailboxsoeren@gmail.com>
2017-12-08 23:03:38 +0100

    responsive create form and delete function

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os  # required to have access to the Port environment variable
+from datetime import datetime  #required for method "now" for file name change
 from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
 from werkzeug.utils import secure_filename  #required for file upload
 from sqlalchemy import create_engine
@@ -12,12 +13,14 @@ from food_database import (Base,
                    Nutrient,
                    ShoppingListItem)
 
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = 'static'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.secret_key = "ABC123"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.add_url_rule('/uploads/<path:filename>', endpoint='uploads',
+                 #view_func=app.send_static_file)
 
 #engine = create_engine('postgresql://vagrant:vagrant@127.0.0.1:5432/np')
 engine = create_engine('postgres://njxqkgsvotldpo:0091ec1051866196d42e608aadc421ef9bb58c37d9fcfe0e7bac4e9ce63929f8@ec2-54-228-182-57.eu-west-1.compute.amazonaws.com:5432/ddjblvctcusagj')
@@ -40,12 +43,15 @@ def showMeals():
 
 @app.route('/meals/add', methods=['GET','POST'])
 def addMeals():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form['button'] == "Save":
         # ---- IMAGE UPLOAD HANDLER -----
+        print("POST button = Save")
+        filename = ""
         if 'file' not in request.files:
-
+            print("file not in request.files")
             # TODO: Implement flash messages
             # flash('No file part')
+
             return redirect(request.url)
 
         file = request.files['file']
@@ -54,20 +60,48 @@ def addMeals():
         if file.filename == '':
             # TODO: Implement flash
             # flash('No selected file')
+            print("Filename == ''")
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            now = datetime.now()
+            filename = "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), filename.rsplit('.', 1)[1])  #$s replaced by timestamp and file extension
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), file.filename.rsplit('.', 1)[1]))
+            print("filename = " + filename)
+            file.save(filepath)
 
         # ---- FORM DATA HANDLER -----
-        
+        newItem = Meal(title=request.form['title'],
+                       description=request.form['description'],
+                       portions=request.form['portions'],
+                       image=filename)
+        session.add(newItem)
+        session.commit()
 
+        return redirect(url_for("showMeals"))
 
-    if request.method == 'GET':
+    elif request.method == 'POST' and request.form['button'] == "Cancel":
+        print("POST button = Cancel")
+        meals = session.query(Meal)
+        return render_template("meals.html",meals=meals,getIngredients=getIngredients)
+    else:  # 'GET':
+        print("GET")
         uoms = session.query(UOM)
         foods = session.query(Food)
         return render_template("meals_add.html",uoms=uoms,foods=foods)
 
+@app.route('/meals/delete/<int:meal_id>', methods=['GET','POST'])
+def deleteMeal(meal_id):
+    o = session.query(Meal).filter_by(id=meal_id).one()
+    if request.method == 'POST' and request.form['button'] == "Delete":
+        session.delete(o)
+        session.commit()
+        #TODO: flash("Meal deleted")
+        return redirect(url_for('showMeals'))
+    elif request.method == 'GET':
+        return render_template("meals_delete.html",meal=o)
+    else:
+        return redirect(url_for("showMeals"))
 
 def getIngredients(meal_id):
     items = session.query(Ingredient).filter_by(meal_id=meal_id)
